bc6138b1ff6624633b63b08c75a4586c181c6585
AjayFCS <127070570+AjayFCS@users.noreply.github.com>
2024-02-22 00:19:18 +0530

    added favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
- 
+import { Html, Head, Main, NextScript } from "next/document";
+
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel="icon" href="/manukula_logo.jpg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
